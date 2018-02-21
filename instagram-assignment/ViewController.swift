12be7d9c0b473be86fe1d658961d0a8a498874a3
lesley amezcua <lesleyamezcua@lesley-amezcuas-MacBook-Pro.local>
2018-02-21 15:12:25 -0800

    Initial Commit

---
+++ instagram-assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagram-assignment
+//
+//  Created by lesley amezcua on 2/21/18.
+//  Copyright © 2018 lesley amezcua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
